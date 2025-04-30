00995c27cae60e104aec6159b609966edcaf5b30
Shreeram Mahadik <shree17998@gmail.com>
2025-04-30 09:58:17 +0530

    First Commit 30/04/2025

---
+++ todo app/todo-app/src/main/java/com/app/todo/app/controller/TaskController.java
@@ -0,0 +1,44 @@
+package com.app.todo.app.controller;
+
+
+import com.app.todo.app.models.Task;
+import com.app.todo.app.services.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks=taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";  //return view name i.e tasks.html under resources --> templates --> tasks.html
+    }
+
+    @PostMapping
+    public String createTask(@RequestParam String title){
+        taskService.createTask(title);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id){
+        taskService.toggleTask(id);
+        return "redirect:/";
+    }
+}
